0ca73e3aaa1d3521f4bdc4ebfbd3815ce1810e42
ZhrDemir <zdemir2702@gmail.com>
2023-10-27 01:56:59 +0300

    20231027

--- src/week_08/P05_listOdev1.java
+++ src/week_08/P05_listOdev1.java
@@ -1,5 +1,9 @@
 package week_08;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class P05_listOdev1 {
     public static void main(String[] args) {
 
@@ -11,6 +15,32 @@ liste olarak dondurecek bir method olusturun.
 
 
  */
+
+        isimleriGetir();
+
+    }
+
+    private static void isimleriGetir() {
+
+        boolean flag= true;
+        List<String> isimler = new ArrayList<>();
+
+        do {
+
+            System.out.println("Lütfen isim girin.Q'ya basarak listeleyebilirsiniz.");
+            Scanner scanner = new Scanner(System.in);
+            String isim = scanner.nextLine();
+
+            if (!isim.equalsIgnoreCase("q")) {
+                isimler.add(isim);
+            } else {
+                System.out.println(isimler);
+                flag = false;
+            }
+        }    while (flag) ;
+
+
+
 
     }
 
--- src/week_08/P06_odev2.java
+++ src/week_08/P06_odev2.java
@@ -1,5 +1,9 @@
 package week_08;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class P06_odev2 {
     public static void main(String[] args) {
         /*
@@ -9,5 +13,30 @@ Kullanicidan pozitif bir tamsayi alip, o tamsayiyi tam bolebilen tum pozitif
 tamsayilari bir liste olarak bize donduren bir method olusturun.
 
          */
+
+        bolenSayilar();
+
+
+
+
+
+
+
+    }
+
+    private static void bolenSayilar() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen pozitif tam sayi giriniz.");
+        int sayi = scanner.nextInt();
+
+        List<Integer> yeniList = new ArrayList<>();
+
+        for (int i = 1; i <=sayi ; i++) {
+            if (sayi%i==0){
+                yeniList.add(i);
+            }
+        }
+        System.out.println(yeniList);
     }
 }
